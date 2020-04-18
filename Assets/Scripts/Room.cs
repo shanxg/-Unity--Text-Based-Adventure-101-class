00b30db430e8cff7da8f63463430f219e7fe5913
Lucas Marchetto Rivaldo <63316622+shanxg@users.noreply.github.com>
2020-04-18 11:00:04 -0300

    Add files via upload
    These are the files for the complete  Unity Text-Adventure Part 1 Class.

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="textAdventure/Room")]
+public class Room : ScriptableObject
+{
+    [TextArea]
+    public string description;
+    public string roomName;
+    public Exit[] exits;
+    
+
+
+}
