4a41ff26a2a8548f3f1a0962f24eab854e7acd4f
Lucas Marchetto Rivaldo <63316622+shanxg@users.noreply.github.com>
2020-04-22 10:02:16 -0300

    Add files via upload
    These are the files for the whole text adventure class.

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="textAdventure/Interactable Object")]
+public class InteractableObject : ScriptableObject
+{
+    public string noun = "name";
+    [TextArea]
+    public string description = "Descripiton in room";
+    public Interaction[] interactions;
+}
---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Interaction 
+{
+    public InputAction inputAction;
+    [TextArea]
+    public string textResponse;
+    public ActionResponse actionResponse;
+
+}
